a2cb87c5fc9e4402637964e6cd492a88214f3139
we77465 <68180138+we77465@users.noreply.github.com>
2023-10-22 15:22:52 +0800

    創建done

--- RedxYo/Create/views.py
+++ RedxYo/Create/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+#def showtemplate(request):
+#    # 今天先不探討什麼是 render，先記得它會去撈 test.html
+#    return render(request, 'test.html')
+
+
+
+from .models import Account
+# # Create your views here.
+# def showtemplate(request):
+#     Account_list = Account.objects.all() # 把所有 Vendor 的資料取出來
+#     context = {'Account_list': Account_list} # 建立 Dict對應到Vendor的資料，
+#     return render(request, 'Creates/create_tetail.html', context)
+
+from .forms import AccountForm
 def showtemplate(request):
-    # 今天先不探討什麼是 render，先記得它會去撈 test.html
-    return render(request, 'test.html')
+    form = AccountForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+
+    context = {
+        'form' : form
+    } # 建立 Dict對應到Vendor的資料，
+    return render(request, 'Creates/1022_test.html', context)
