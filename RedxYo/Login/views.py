6e7f8c7e11285b8c642caa4ccb315db754656816
we77465 <68180138+we77465@users.noreply.github.com>
2023-10-22 23:25:04 +0800

    url寫死
    垃圾東西 我搞不出來

--- RedxYo/Login/views.py
+++ RedxYo/Login/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .forms import LoginForm
 #首頁
-def index(request):
-    return render(request, 'Logins/index.html')
+#def index(request):
+#    return render(request, 'Logins/index.html')
 
+def sign_in(request):
+    form = LoginForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'Logins/index.html', context)
