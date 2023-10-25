95229a3e4fa5c25e5987fca6d36df33719e896a5
we77465 <68180138+we77465@users.noreply.github.com>
2023-10-25 15:22:57 +0800

    copy_master

--- RedxYo/Create/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Account,AccountAdmin
-
-# Register your models here.
-admin.site.register(Account,AccountAdmin)
