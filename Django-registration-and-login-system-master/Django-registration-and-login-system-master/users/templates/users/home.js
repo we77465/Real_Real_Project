80beafe163a5ce06d3622220faed324d9a773775
we77465 <68180138+we77465@users.noreply.github.com>
2023-11-02 22:14:08 +0800

    怪怪2

--- Django-registration-and-login-system-master/Django-registration-and-login-system-master/users/templates/users/home.js
+++ Django-registration-and-login-system-master/Django-registration-and-login-system-master/users/templates/users/home.js
@@ -4,15 +4,14 @@ document.addEventListener("DOMContentLoaded", function() {
     downloadButtons.forEach(function(button) {
         button.addEventListener("click", function() {
             const imageUrl = button.getAttribute("data-url");
-            const imageId = button.getAttribute("data-image-id");
-
-            // 发送HTTP请求到Django视图，并将image_id传递过去
-            fetch(`/download-image/${imageId}/`)
+            
+            // 发送HTTP请求到Django视图
+            fetch(`/download-image/?image_url=${encodeURIComponent(imageUrl)}`)
                 .then(response => {
                     if (response.ok) {
                         return response.blob();
                     }
-                    throw new Error('error');
+                    throw new Error('网络请求失败');
                 })
                 .then(blob => {
                     const url = window.URL.createObjectURL(blob);
@@ -28,28 +27,3 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
-
-
-function confirmDelete(button) {
-    const imageId = button.getAttribute("data-image-id");
-    if (confirm("確定刪除?")) {
-        deleteImage(imageId, button);
-    }
-    event.preventDefault(); // 阻止默認事件
-}
-
-function deleteImage(imageId, button) {
-    fetch(`/delete-image/${imageId}/`)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error('刪除失敗');
-        })
-        .then(data => {
-            alert(data.message); 
-        })
-        .catch(error => {
-            console.error(error);
-        });
-}
